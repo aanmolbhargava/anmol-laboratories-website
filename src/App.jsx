6ef3e1d7d3343699e9dd4b2759670ac909bb9f18
Ipsita Arora <ipsitaarora6@gmail.com>
2026-07-21 00:20:46 +0530

    Fixed App.jsx imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import AppRoutes from "./routes/AppRoutes";
-import FloatingWhatsApp from "./components/common/FloatingWhatsApp";
+import FloatingWhatsApp from "./components/common/FloatingWhatsapp";
 import ScrollTop from "./components/common/ScrollTop";
 import SearchModal from "./components/search/SearchModal";
 function App() {
